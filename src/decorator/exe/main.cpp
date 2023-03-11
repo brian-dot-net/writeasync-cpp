a0e9cdab793750fbe83b6c4c275414509abac188
Brian Rogers <bobbymcr@gmail.com>
2023-03-11 09:01:55 -0800

    Use coin_trace decorators in sample app

--- src/decorator/exe/main.cpp
+++ src/decorator/exe/main.cpp
@@ -2,30 +2,13 @@
 #include <sstream>
 #include <iostream>
 
-#include "coin.h"
-#include "trace.h"
+#include "coin_trace.h"
 
-void flip_one(wacpp::ICoin& coin, wacpp::ITracer& tracer)
+void flip_many(wacpp::ICoin& coin)
 {
-    if (coin.flip())
-    {
-        tracer.trace("It was heads!");
-    }
-    else
-    {
-        tracer.trace("It was tails!");
-    }
-}
-
-void flip_many(wacpp::ICoin& coin, wacpp::ITracer& tracer, const std::string& name)
-{
-    std::stringstream ss;
-    ss << "Flipping " << name << "...";
-    tracer.trace(ss.str());
-
     for (int i = 0; i < 10; ++i)
     {
-        flip_one(coin, tracer);
+        coin.flip();
     }
 }
 
@@ -33,11 +16,13 @@ int main()
 {
     auto tracer = wacpp::make_ostream_tracer(std::cout);
 
-    auto coin1 = wacpp::make_double_headed_coin();
-    flip_many(*coin1, *tracer, "TwoHeads");
+    auto inner1 = wacpp::make_double_headed_coin();
+    auto coin1 = wacpp::make_traced_coin(std::move(inner1), tracer, "TwoHeads");
+    flip_many(*coin1);
 
-    auto coin2 = wacpp::make_fair_coin();
-    flip_many(*coin2, *tracer, "Fair");
+    auto inner2 = wacpp::make_fair_coin();
+    auto coin2 = wacpp::make_traced_coin(std::move(inner2), tracer, "Fair");
+    flip_many(*coin2);
 
     return 0;
 }
