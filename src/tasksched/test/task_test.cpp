e9df5bd8e6f390000ee9f450287c487ca874a6fd
Brian Rogers <bobbymcr@gmail.com>
2023-07-15 13:53:58 -0700

    Complete set_author test

--- src/tasksched/test/task_test.cpp
+++ src/tasksched/test/task_test.cpp
@@ -16,6 +16,7 @@ struct Stub
     struct Data
     {
         HRESULT get_RegistrationInfo_result{};
+        HRESULT put_Author_result{};
     };
 
     class RegistrationInfo : public wacpp::test::Stub_IRegistrationInfo
@@ -23,10 +24,43 @@ struct Stub
     public:
         RegistrationInfo(const Data& data)
             : m_data(data)
+            , m_author()
         {}
+
+        STDMETHODIMP get_XmlText(
+            BSTR* pXml) noexcept override
+        try
+        {
+            std::wstring xml{};
+            if (!m_author.empty())
+            {
+                xml += std::format(L"<Author>{}</Author>", m_author);
+            }
+
+            auto outer_xml = wil::make_bstr(xml.c_str());
+            *pXml = outer_xml.release();
+
+            return S_OK;
+        }
+        CATCH_RETURN()
+
+        STDMETHODIMP put_Author(
+            BSTR author) noexcept override
+        try
+        {
+            const auto hr = m_data.put_Author_result;
+            if (SUCCEEDED(hr))
+            {
+                m_author = author;
+            }
+
+            return hr;
+        }
+        CATCH_RETURN()
 
     private:
         const Data& m_data;
+        std::wstring m_author;
     };
 
     class TaskDefinition : public wacpp::test::Stub_ITaskDefinition
@@ -34,6 +68,7 @@ struct Stub
     public:
         TaskDefinition(const Data& data)
             : m_data(data)
+            , m_registration_info()
         {}
 
         STDMETHODIMP get_XmlText(
@@ -42,6 +77,14 @@ struct Stub
         {
             std::wstring xml{};
             xml += L"<Task>";
+
+            if (m_registration_info)
+            {
+                wil::unique_bstr str;
+                THROW_IF_FAILED(m_registration_info->get_XmlText(str.put()));
+                xml += str.get();
+            }
+
             xml += L"</Task>";
 
             auto outer_xml = wil::make_bstr(xml.c_str());
@@ -58,8 +101,8 @@ struct Stub
             const auto hr = m_data.get_RegistrationInfo_result;
             if (SUCCEEDED(hr))
             {
-                auto ptr = winrt::make<Stub::RegistrationInfo>(m_data);
-                ptr.copy_to(ppRegistrationInfo);
+                m_registration_info = winrt::make<Stub::RegistrationInfo>(m_data);
+                m_registration_info.copy_to(ppRegistrationInfo);
             }
 
             return hr;
@@ -68,6 +111,7 @@ struct Stub
 
     private:
         const Data& m_data;
+        winrt::com_ptr<IRegistrationInfo> m_registration_info;
     };
 };
 
@@ -93,7 +137,10 @@ namespace wacpp::test
 
 TEST(task_test, set_author)
 {
-    Stub::Data data{.get_RegistrationInfo_result = E_FAIL };
+    Stub::Data data{
+        .get_RegistrationInfo_result = E_FAIL,
+        .put_Author_result = E_FAIL,
+    };
     Task task(make_stub_task_definition(data));
 
     ASSERT_THROW(task.set_author(L"Fail 1"), wil::ResultException);
@@ -105,6 +152,16 @@ TEST(task_test, set_author)
     ASSERT_THROW(task.set_author(L"Fail 2"), wil::ResultException);
 
     assert_xml(task, L"<Task></Task>");
+
+    data.put_Author_result = S_OK;
+
+    task.set_author(L"Some Author");
+
+    const auto expected =
+        L"<Task>"
+        L"<Author>Some Author</Author>"
+        L"</Task>";
+    assert_xml(task, expected);
 }
 
 }
