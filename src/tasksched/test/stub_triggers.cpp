91a0d1464682b87877328784e4c2e3ff3c3ab583
Brian Rogers <bobbymcr@gmail.com>
2023-07-22 09:48:35 -0700

    Create stub for ITimeTrigger

--- src/tasksched/test/stub_triggers.cpp
+++ src/tasksched/test/stub_triggers.cpp
@@ -3,6 +3,133 @@
 namespace wacpp::test
 {
 
+STDMETHODIMP Stub_ITimeTrigger::get_RandomDelay(
+    [[maybe_unused]] BSTR* pRandomDelay) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_RandomDelay(
+    [[maybe_unused]] BSTR randomDelay) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_Type(
+    [[maybe_unused]] TASK_TRIGGER_TYPE2* pType) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_Id(
+    [[maybe_unused]] BSTR* pId) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_Id(
+    [[maybe_unused]] BSTR id) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_Repetition(
+    [[maybe_unused]] IRepetitionPattern** ppRepeat) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_Repetition(
+    [[maybe_unused]] IRepetitionPattern* pRepeat) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_ExecutionTimeLimit(
+    [[maybe_unused]] BSTR* pTimeLimit) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_ExecutionTimeLimit(
+    [[maybe_unused]] BSTR timelimit) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_StartBoundary(
+    [[maybe_unused]] BSTR* pStart) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_StartBoundary(
+    [[maybe_unused]] BSTR start) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_EndBoundary(
+    [[maybe_unused]] BSTR* pEnd) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_EndBoundary(
+    [[maybe_unused]] BSTR end) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::get_Enabled(
+    [[maybe_unused]] VARIANT_BOOL* pEnabled) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::put_Enabled(
+    [[maybe_unused]] VARIANT_BOOL enabled) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::GetTypeInfoCount(
+    [[maybe_unused]] UINT* pctinfo) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::GetTypeInfo(
+    [[maybe_unused]] UINT iTInfo,
+    [[maybe_unused]] LCID lcid,
+    [[maybe_unused]] ITypeInfo** ppTInfo) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::GetIDsOfNames(
+    [[maybe_unused]] REFIID riid,
+    [[maybe_unused]] LPOLESTR* rgszNames,
+    [[maybe_unused]] UINT cNames,
+    [[maybe_unused]] LCID lcid,
+    [[maybe_unused]] DISPID* rgDispId) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_ITimeTrigger::Invoke(
+    [[maybe_unused]] DISPID dispIdMember,
+    [[maybe_unused]] REFIID riid,
+    [[maybe_unused]] LCID lcid,
+    [[maybe_unused]] WORD wFlags,
+    [[maybe_unused]] DISPPARAMS* pDispParams,
+    [[maybe_unused]] VARIANT* pVarResult,
+    [[maybe_unused]] EXCEPINFO* pExcepInfo,
+    [[maybe_unused]] UINT* puArgErr) noexcept
+{
+    return E_NOTIMPL;
+}
+
 STDMETHODIMP Stub_ITriggerCollection::get_Count(
     [[maybe_unused]] long* pCount) noexcept
 {
--- src/tasksched/test/stub_triggers.h
+++ src/tasksched/test/stub_triggers.h
@@ -8,6 +8,87 @@
 namespace wacpp::test
 {
 
+class Stub_ITimeTrigger : public winrt::implements<Stub_ITimeTrigger, ITimeTrigger>
+{
+public:
+    // ITimeTrigger
+
+    STDMETHODIMP get_RandomDelay(
+        BSTR* pRandomDelay) noexcept override;
+
+    STDMETHODIMP put_RandomDelay(
+        BSTR randomDelay) noexcept override;
+
+    // ITrigger
+
+    STDMETHODIMP get_Type(
+        TASK_TRIGGER_TYPE2* pType) noexcept override;
+
+    STDMETHODIMP get_Id(
+        BSTR* pId) noexcept override;
+
+    STDMETHODIMP put_Id(
+        BSTR id) noexcept override;
+
+    STDMETHODIMP get_Repetition(
+        IRepetitionPattern** ppRepeat) noexcept override;
+
+    STDMETHODIMP put_Repetition(
+        IRepetitionPattern* pRepeat) noexcept override;
+
+    STDMETHODIMP get_ExecutionTimeLimit(
+        BSTR* pTimeLimit) noexcept override;
+
+    STDMETHODIMP put_ExecutionTimeLimit(
+        BSTR timelimit) noexcept override;
+
+    STDMETHODIMP get_StartBoundary(
+        BSTR* pStart) noexcept override;
+
+    STDMETHODIMP put_StartBoundary(
+        BSTR start) noexcept override;
+
+    STDMETHODIMP get_EndBoundary(
+        BSTR* pEnd) noexcept override;
+
+    STDMETHODIMP put_EndBoundary(
+        BSTR end) noexcept override;
+
+    STDMETHODIMP get_Enabled(
+        VARIANT_BOOL* pEnabled) noexcept override;
+
+    STDMETHODIMP put_Enabled(
+        VARIANT_BOOL enabled) noexcept override;
+
+    // IDispatch
+
+    STDMETHODIMP GetTypeInfoCount(
+        UINT* pctinfo) noexcept override;
+
+    STDMETHODIMP GetTypeInfo(
+        UINT iTInfo,
+        LCID lcid,
+        ITypeInfo** ppTInfo) noexcept override;
+
+    STDMETHODIMP GetIDsOfNames(
+        REFIID riid,
+        LPOLESTR* rgszNames,
+        UINT cNames,
+        LCID lcid,
+        DISPID* rgDispId) noexcept override;
+
+    STDMETHODIMP Invoke(
+        DISPID dispIdMember,
+        REFIID riid,
+        LCID lcid,
+        WORD wFlags,
+        DISPPARAMS* pDispParams,
+        VARIANT* pVarResult,
+        EXCEPINFO* pExcepInfo,
+        UINT* puArgErr) noexcept override;
+
+};
+
 class Stub_ITriggerCollection : public winrt::implements<Stub_ITriggerCollection, ITriggerCollection>
 {
 public:
