0b20ba302c31467274081e1cb0d071cb1136ab32
Brian Rogers <bobbymcr@gmail.com>
2023-07-22 16:12:50 -0700

    Create stub for IExecAction

--- src/tasksched/test/stub_actions.cpp
+++ src/tasksched/test/stub_actions.cpp
@@ -3,6 +3,97 @@
 namespace wacpp::test
 {
 
+STDMETHODIMP Stub_IExecAction::get_Path(
+    [[maybe_unused]] BSTR* pPath) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::put_Path(
+    [[maybe_unused]] BSTR path) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::get_Arguments(
+    [[maybe_unused]] BSTR* pArgument) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::put_Arguments(
+    [[maybe_unused]] BSTR argument) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::get_WorkingDirectory(
+    [[maybe_unused]] BSTR* pWorkingDirectory) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::put_WorkingDirectory(
+    [[maybe_unused]] BSTR workingDirectory) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::get_Id(
+    [[maybe_unused]] BSTR* pId) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::put_Id(
+    [[maybe_unused]] BSTR Id) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::get_Type(
+    [[maybe_unused]] TASK_ACTION_TYPE* pType) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::GetTypeInfoCount(
+    [[maybe_unused]] UINT* pctinfo) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::GetTypeInfo(
+    [[maybe_unused]] UINT iTInfo,
+    [[maybe_unused]] LCID lcid,
+    [[maybe_unused]] ITypeInfo** ppTInfo) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::GetIDsOfNames(
+    [[maybe_unused]] REFIID riid,
+    [[maybe_unused]] LPOLESTR* rgszNames,
+    [[maybe_unused]] UINT cNames,
+    [[maybe_unused]] LCID lcid,
+    [[maybe_unused]] DISPID* rgDispId) noexcept
+{
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP Stub_IExecAction::Invoke(
+    [[maybe_unused]] DISPID dispIdMember,
+    [[maybe_unused]] REFIID riid,
+    [[maybe_unused]] LCID lcid,
+    [[maybe_unused]] WORD wFlags,
+    [[maybe_unused]] DISPPARAMS* pDispParams,
+    [[maybe_unused]] VARIANT* pVarResult,
+    [[maybe_unused]] EXCEPINFO* pExcepInfo,
+    [[maybe_unused]] UINT* puArgErr) noexcept
+{
+    return E_NOTIMPL;
+}
+
 STDMETHODIMP Stub_IActionCollection::get_Count(
     [[maybe_unused]] long* pCount) noexcept
 {
--- src/tasksched/test/stub_actions.h
+++ src/tasksched/test/stub_actions.h
@@ -8,6 +8,67 @@
 namespace wacpp::test
 {
 
+class Stub_IExecAction : public winrt::implements<Stub_IExecAction, IExecAction>
+{
+    // IExecAction
+
+    STDMETHODIMP get_Path(
+        BSTR* pPath) noexcept override;
+
+    STDMETHODIMP put_Path(
+        BSTR path) noexcept override;
+
+    STDMETHODIMP get_Arguments(
+        BSTR* pArgument) noexcept override;
+
+    STDMETHODIMP put_Arguments(
+        BSTR argument) noexcept override;
+
+    STDMETHODIMP get_WorkingDirectory(
+        BSTR* pWorkingDirectory) noexcept override;
+
+    STDMETHODIMP put_WorkingDirectory(
+        BSTR workingDirectory) noexcept override;
+
+    // IAction
+
+    STDMETHODIMP get_Id(
+        BSTR* pId) noexcept override;
+
+    STDMETHODIMP put_Id(
+        BSTR Id) noexcept override;
+
+    STDMETHODIMP get_Type(
+        TASK_ACTION_TYPE* pType) noexcept override;
+
+    // IDispatch
+
+    STDMETHODIMP GetTypeInfoCount(
+        UINT* pctinfo) noexcept override;
+
+    STDMETHODIMP GetTypeInfo(
+        UINT iTInfo,
+        LCID lcid,
+        ITypeInfo** ppTInfo) noexcept override;
+
+    STDMETHODIMP GetIDsOfNames(
+        REFIID riid,
+        LPOLESTR* rgszNames,
+        UINT cNames,
+        LCID lcid,
+        DISPID* rgDispId) noexcept override;
+
+    STDMETHODIMP Invoke(
+        DISPID dispIdMember,
+        REFIID riid,
+        LCID lcid,
+        WORD wFlags,
+        DISPPARAMS* pDispParams,
+        VARIANT* pVarResult,
+        EXCEPINFO* pExcepInfo,
+        UINT* puArgErr) noexcept override;
+};
+
 class Stub_IActionCollection : public winrt::implements<Stub_IActionCollection, IActionCollection>
 {
 public:
