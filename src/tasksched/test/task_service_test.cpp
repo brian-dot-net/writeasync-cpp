ccd7e2c12a98110359c484a12db8caecfab439ba
Brian Rogers <bobbymcr@gmail.com>
2023-07-26 23:07:23 -0700

    basic_test allows GetFolder

--- src/tasksched/test/task_service_test.cpp
+++ src/tasksched/test/task_service_test.cpp
@@ -5,6 +5,7 @@
 #include <wil/com.h>
 
 #include "stub_tasksvc.h"
+#include "stub_taskfolder.h"
 #include "task_service.h"
 
 namespace
@@ -15,6 +16,29 @@ struct Stub
     struct TaskServiceData
     {
         HRESULT Connect_result{};
+        HRESULT GetFolder_result{};
+    };
+
+    class ComTaskFolder : public wacpp::test::Stub_ITaskFolder
+    {
+    public:
+        ComTaskFolder(const std::wstring& path)
+            : m_path(path)
+        {
+        }
+
+        STDMETHODIMP get_Path(
+            BSTR* pPath) noexcept override
+        try
+        {
+            auto path = wil::make_bstr(m_path.c_str());
+            *pPath = path.release();
+            return S_OK;
+        }
+        CATCH_RETURN()
+
+    private:
+        std::wstring m_path;
     };
 
     class ComTaskService : public wacpp::test::Stub_ITaskService
@@ -26,6 +50,22 @@ struct Stub
             : m_data(data)
             , m_connected()
         {}
+
+        STDMETHODIMP GetFolder(
+            BSTR path,
+            ITaskFolder** ppFolder) noexcept override
+        try
+        {
+            const auto hr = m_data.GetFolder_result;
+            if (SUCCEEDED(hr))
+            {
+                auto folder = winrt::make<ComTaskFolder>(path);
+                folder.copy_to(ppFolder);
+            }
+
+            return hr;
+        }
+        CATCH_RETURN()
 
         STDMETHODIMP Connect(
             VARIANT serverName,
@@ -77,6 +117,15 @@ void assert_connected(wacpp::TaskService& service)
     ASSERT_EQ(VARIANT_TRUE, b);
 }
 
+void assert_path(wacpp::TaskFolder& folder, const std::wstring& expected)
+{
+    wil::unique_bstr str{};
+    THROW_IF_FAILED(folder.get().get_Path(str.put()));
+    std::wstring actual = str.get();
+
+    ASSERT_EQ(expected, actual);
+}
+
 }
 
 namespace wacpp::test
@@ -85,7 +134,8 @@ namespace wacpp::test
 TEST(task_service_test, basic_test)
 {
     Stub::TaskServiceData data{
-        .Connect_result = E_FAIL
+        .Connect_result = E_FAIL,
+        .GetFolder_result = E_FAIL,
     };
 
     ASSERT_THROW(make_stub_task_service(data), wil::ResultException);
@@ -94,6 +144,13 @@ TEST(task_service_test, basic_test)
 
     auto service = make_stub_task_service(data);
     assert_connected(service);
+
+    ASSERT_THROW(service.get_root_folder(), wil::ResultException);
+
+    data.GetFolder_result = S_OK;
+
+    auto folder = service.get_root_folder();
+    assert_path(folder, L"\\");
 }
 
 }
