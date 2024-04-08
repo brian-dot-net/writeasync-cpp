2003832b9b560a6cf1fd944f5d4864d0f3314e27
Brian Rogers <bobbymcr@gmail.com>
2024-04-07 20:35:04 -0700

    dinject - sample exe

--- src/dinject/exe/main.cpp
+++ src/dinject/exe/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 
-#include "config.h"
+#include "config_ser.h"
 
 int main()
 {
     using namespace wacpp;
 
     Config c{};
-    for (const auto& [name, _] : c)
-    {
-        std::cout << name << "\n";
-    }
+
+    ConfigSection s1{ "S1" };
+    s1.insert("K1", "V1");
+    c.insert(std::move(s1));
+
+    ConfigSection s2{ "S2" };
+    s2.insert("K2", "V2");
+    c.insert(std::move(s2));
+
+    ConfigSectionIniSerializer css{};
+    ConfigIniSerializer cs{ css };
+    cs.serialize(c, std::cout);
 
     return 0;
 }
