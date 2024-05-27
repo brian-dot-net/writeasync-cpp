ba47a908a382a23302e5461bbcab6d42e917215a
Brian Rogers <bobbymcr@gmail.com>
2024-05-27 10:49:19 -0700

    utf16: change benchmark to use stringstream

--- src/utf16/perf/str_perf.cpp
+++ src/utf16/perf/str_perf.cpp
@@ -1,5 +1,7 @@
 #include "str.h"
 
+#include <sstream>
+
 #include <benchmark/benchmark.h>
 
 using namespace wacpp;
@@ -7,11 +9,13 @@ using namespace wacpp;
 static void to_utf8_short_string(benchmark::State& state)
 {
     const auto input = std::wstring{ L"\u4F60\u597D" };
-    auto result = std::string{};
+    auto ss = std::stringstream{};
 
     for (auto _ : state)
     {
-        result = str::to_utf8(input);
+        ss.clear();
+        const auto result = str::to_utf8(input);
+        ss << result;
     }
 }
 
@@ -20,11 +24,13 @@ BENCHMARK(to_utf8_short_string);
 static void to_utf8_long_string(benchmark::State& state)
 {
     const auto input = std::wstring{ L"\u4F60\u597D\uFF01\u4F60\u4F1A\u8BF4\u4E2D\u6587\u5417\uFF1F\u4E00\u4F1A\u513F\u89C1\u3002" };
-    auto result = std::string{};
+    auto ss = std::stringstream{};
 
     for (auto _ : state)
     {
-        result = str::to_utf8(input);
+        ss.clear();
+        const auto result = str::to_utf8(input);
+        ss << result;
     }
 }
 
