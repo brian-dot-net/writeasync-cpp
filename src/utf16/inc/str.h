c1abac1a29b27a1fc57c4efd792a3e7c0af7aee4
Brian Rogers <bobbymcr@gmail.com>
2024-05-27 11:04:22 -0700

    utf16: add block-based conversion

--- src/utf16/inc/str.h
+++ src/utf16/inc/str.h
@@ -1,9 +1,161 @@
 #pragma once
 
 #include <string>
+#include <string_view>
+
+#include <gsl/gsl>
+#include <wil/result_macros.h>
+
+namespace wacpp::str::detail
+{
+
+template <size_t N>
+    requires (N > 3)
+class Utf8Range
+{
+public:
+    using Input = std::wstring_view;
+    using Output = gsl::span<char>;
+    class Iterator;
+    struct Sentinel {};
+
+    Utf8Range(Input input) noexcept : m_input{ input }, m_block{}
+    {}
+
+    Iterator begin()
+    {
+        return { m_input, Output{m_block} };
+    }
+
+    Sentinel end()
+    {
+        return {};
+    }
+
+    class Iterator
+    {
+    public:
+        using difference_type = std::ptrdiff_t;
+        using value_type = char;
+
+        Iterator() noexcept : m_source{}, m_next_offset{}, m_block{}, m_index{}
+        {}
+
+        Iterator(Input source, Output block)
+            : m_source{ source }, m_next_offset{}, m_block{ block }, m_index{ m_block.size() - 1 }
+        {
+            move_next();
+        }
+
+        char operator*() const
+        {
+            return m_block[m_index];
+        }
+
+        Iterator& operator++()
+        {
+            if (!at_end())
+            {
+                move_next_char();
+            }
+
+            return *this;
+        }
+
+        Iterator operator++(int)
+        {
+            Iterator prev = *this;
+            ++*this;
+            return prev;
+        }
+
+        bool operator==(Sentinel) const noexcept
+        {
+            return at_end();
+        }
+
+    private:
+        bool at_block_end() const noexcept
+        {
+            return m_index == m_block.size() - 1;
+        }
+
+        bool at_end() const noexcept
+        {
+            return (m_next_offset >= m_source.size()) && at_block_end();
+        }
+
+        void move_next_char()
+        {
+            if (!at_block_end())
+            {
+                ++m_index;
+            }
+            else
+            {
+                move_next();
+            }
+        }
+
+        void move_next()
+        {
+            if (at_end())
+            {
+                return;
+            }
+
+            const auto hr = try_move_next();
+            THROW_IF_FAILED_MSG(hr, "Error near offset %llu while attempting UTF-8 conversion", m_next_offset);
+
+            m_index = 0;
+        }
+
+        HRESULT try_move_next()
+        {
+            const auto input_block = next_block();
+            const auto input_size = gsl::narrow<int>(input_block.size());
+            static constexpr const auto output_size = gsl::narrow<int>(N);
+            const auto actual_size = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, input_block.data(), input_size, m_block.data(), output_size, nullptr, nullptr);
+            if (actual_size == 0)
+            {
+                return HRESULT_FROM_WIN32(GetLastError());
+            }
+
+            m_block = m_block.subspan(0, actual_size);
+            m_next_offset += actual_size;
+            return S_OK;
+        }
+
+        auto next_block() const
+        {
+            static constexpr const auto M = (N / 4);
+            const auto max_input_size = m_source.size() - m_next_offset;
+            const auto input_size = (M <= max_input_size) ? M : max_input_size;
+            return m_source.substr(m_next_offset, input_size);
+        }
+
+        Input m_source;
+        size_t m_next_offset;
+        Output m_block;
+        size_t m_index;
+    };
+
+private:
+    Input m_input;
+    std::array<char, N> m_block;
+};
+
+}
 
 namespace wacpp::str
 {
+
+template <size_t N>
+    requires (N > 3)
+detail::Utf8Range<N> to_utf8(const std::wstring& utf16_input)
+{
+    return { std::wstring_view{utf16_input} };
+}
 
 std::string to_utf8(const std::wstring& utf16_input);
 
--- src/utf16/test/str_test.cpp
+++ src/utf16/test/str_test.cpp
@@ -6,6 +6,53 @@
 
 using namespace wacpp;
 
+template <size_t N>
+    requires (N > 3)
+static void test_utf8_block(const std::wstring& input, const std::string& expected)
+{
+    auto ss = std::stringstream{};
+
+    for (const auto c : str::to_utf8<N>(input))
+    {
+        ss << c;
+    }
+
+    ASSERT_EQ(expected, str::to_utf8(input));
+}
+
+template <size_t N>
+    requires (N > 3)
+static void fail_utf8_block(const std::wstring& input)
+{
+    auto fail = [&](const std::wstring& s) {
+        auto ss = std::stringstream{};
+
+        for (const auto c : str::to_utf8<N>(s))
+        {
+            ss << c;
+        }
+        };
+
+    ASSERT_THROW(fail(input), wil::ResultException);
+}
+
+TEST(to_utf8_test, basic_block)
+{
+    const auto input = std::wstring{
+        L"\u4F60\u597D\uFF01\u4F60\u4F1A\u8BF4\u4E2D\u6587"
+        L"\u5417\uFF1F\u4E00\u4F1A\u513F\u89C1\u3002" };
+    const auto expected = std::string{
+        "\xE4\xBD\xA0\xE5\xA5\xBD\xEF\xBC\x81\xE4\xBD\xA0\xE4\xBC\x9A\xE8"
+        "\xAF\xB4\xE4\xB8\xAD\xE6\x96\x87\xE5\x90\x97\xEF\xBC\x9F\xE4\xB8"
+        "\x80\xE4\xBC\x9A\xE5\x84\xBF\xE8\xA7\x81\xE3\x80\x82"
+    };
+
+    test_utf8_block<4>(input, expected);
+    test_utf8_block<9>(input, expected);
+    test_utf8_block<14>(input, expected);
+    test_utf8_block<19>(input, expected);
+}
+
 TEST(to_utf8_test, basic)
 {
     const auto input = std::wstring{
@@ -34,6 +81,14 @@ TEST(to_utf8_test, invalid_input)
     const auto input = std::wstring{ reinterpret_cast<const wchar_t*>(bad.data()) };
 
     ASSERT_THROW(str::to_utf8(input), wil::ResultException);
+}
+
+TEST(to_utf8_test, invalid_input_block)
+{
+    static constexpr const auto bad = std::array<uint16_t, 2>{0xD800, 0x0000};
+    const auto input = std::wstring{ reinterpret_cast<const wchar_t*>(bad.data()) };
+
+    fail_utf8_block<4>(input);
 }
 
 TEST(to_utf8_test, emoji)
